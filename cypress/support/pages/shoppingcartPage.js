2d803f053fbfa463f5ca9e6edd26798dfbf31958
mjorabdo <jorabdo1991@gmail.com>
2023-04-19 23:37:33 +0800

    eliminando timeout

--- cypress/support/pages/shoppingcartPage.js
+++ cypress/support/pages/shoppingcartPage.js
@@ -8,12 +8,12 @@ checkNameProduct(product){
 };
 
 checkPriceProduct(price){
-  const timeout = 10000;
+  
   return cy.get(`[name='${price}']`)
 }
 
 checkProductRelatesToPrice(product){
- const timeout = 10000;
+ 
  cy.contains(product).siblings('#productPrice')
 }
 }
